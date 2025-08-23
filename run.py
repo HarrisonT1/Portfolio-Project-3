b85b1d5ab6fa07c136941a7b4919a05a896ae6ac
HarrisonT1 <harrisontownend@hotmail.com>
2025-08-23 23:50:16 +0100

    change x-axis numbers to letters for alignment

--- run.py
+++ run.py
@@ -5,13 +5,24 @@ def create_grid():
     print("Please enter an whole number")
     grid_wdith = int(input("How wide would you like your grid to be: "))
     grid = []
+    cell_width = 4
+
     for i in range(grid_wdith):
-        grid.append(["■"] * grid_wdith)
+        grid.append([f"{'■':{cell_width}}"] * grid_wdith)
     #   this print statement and for loop creates the numbered axis x and y to
     #   make it easier to select specific tiles.
-    print("  " + " ".join(str(x) for x in range(grid_wdith)))
+    # X axis
+    print("     ", end="")
+    for x in range(grid_wdith):
+        letter = chr(ord('A') + x)
+        print(f"{letter:{cell_width}}", end="")
+    print()
+    # Y axis
     for y, row in enumerate(grid):
-        print(f"{y:3} " + " ".join(row))
+        print(f"{y+1:4} ", end="")
+        for cell in row:
+            print(f"{cell:3}", end="")
+        print()
     return grid
 
 
