f142758412bdf2561e7606b0a4364af0bbf83b78
HarrisonT1 <harrisontownend@hotmail.com>
2025-08-23 15:19:11 +0100

    Add axis numbers to the grid

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 def create_grid():
+    """
+    This function will create a grid based on what the user inputs
+    """
     print("Please enter an whole number")
     grid_wdith = int(input("How wide would you like your grid to be: "))
     grid = []
     for i in range(grid_wdith):
         grid.append(["■"] * grid_wdith)
-    for row in grid:
-        print(" ".join(row))
+    #   this print statement and for loop creates the numbered axis x and y to make it easier to select specific tiles.
+    print("  " + " ".join(str(x) for x in range(grid_wdith)))
+    for y, row in enumerate(grid):
+        print(f"{y} " + " ".join(row))
     return grid
 
 
