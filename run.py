c75117d434280f27014aa4c97cb4eaf2fae67073
HarrisonT1 <harrisontownend@hotmail.com>
2025-08-23 18:21:35 +0100

    Add :3 to reserve 3 spaces for the axis number to fix grids of more than 10

--- run.py
+++ run.py
@@ -7,10 +7,11 @@ def create_grid():
     grid = []
     for i in range(grid_wdith):
         grid.append(["■"] * grid_wdith)
-    #   this print statement and for loop creates the numbered axis x and y to make it easier to select specific tiles.
+    #   this print statement and for loop creates the numbered axis x and y to
+    #   make it easier to select specific tiles.
     print("  " + " ".join(str(x) for x in range(grid_wdith)))
     for y, row in enumerate(grid):
-        print(f"{y} " + " ".join(row))
+        print(f"{y:3} " + " ".join(row))
     return grid
 
 
