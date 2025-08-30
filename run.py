eb06ab4ccfb310b707c4f36423ad00b6b75ea6cd
HarrisonT1 <harrisontownend@hotmail.com>
2025-08-30 12:18:05 +0100

    Add google spreadsheet and test if its linked

--- run.py
+++ run.py
@@ -1,7 +1,24 @@
+import gspread
+from google.oauth2.service_account import Credentials
 import random
 import os
 from colorama import init, Fore, Style
 init(autoreset=True)
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Project-Portfolio-3')
+
+stats = SHEET.worksheet('stats')
+data = stats.get_all_values()
+
 
 
 # imported from stackoverflow.com - see readme
